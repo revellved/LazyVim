2c68e90e6ffe878591c8c6f80427167cf8e529b7
Ruslan D. Revellved <revellved@gmail.com>
2024-08-28 09:09:45 +0500

    (LAZYVIM) 🍩 Update

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd([[
+  let g:languagetool_server = "/usr/share/java/languagetool/languagetool-server.jar"
+  let g:languagetool_port = "8081"
+]])
---
+++ lua/plugins/core.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+
+  {
+    "vigoux/LanguageTool.nvim",
+    -- opts = {
+    --   languagetool_server = "/usr/share/java/languagetool",
+    --   languagetool_server_jar = "/usr/share/java/languagetool/languagetool-server.jar",
+    --   languagetool_port = "8081",
+    -- },
+    -- opts = function()
+    --   vim.opt_global.languagetool_server = "/usr/share/java/languagetool/languagetool-server.jar"
+    --   vim.opt_local.languagetool_server = "/usr/share/java/languagetool/languagetool-server.jar"
+    --   vim.opt_local.languagetool_port = "8081"
+    --   vim.opt_global.languagetool_port = "8081"
+    -- end,
+  },
+}
